33e1bd5110ed2a630741f0fcf6d29b1cbd6db5fc
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-10-28 15:35:10 +0200

    Navbar and Home

---
+++ src/components/DishComments.tsx
@@ -0,0 +1,27 @@
+import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import { Dish, Comment } from "../types/index"
+
+
+interface DishCommentsProps {
+  selectedPasta: Dish | null;
+}
+
+const DishComments = ({ selectedPasta }: DishCommentsProps) => (
+  <Container>
+    <Row className="justify-content-center mt-3">
+      <Col xs={12} md={6}>
+        <ListGroup>
+          {selectedPasta ? (
+            selectedPasta.comments.map((c) => (
+              <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+            ))
+          ) : (
+            <p>Click on a dish to read its comments</p>
+          )}
+        </ListGroup>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default DishComments;
---
+++ src/components/MyNav.tsx
@@ -0,0 +1,30 @@
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
+
+interface MyNavProps {
+    title: string
+}
+
+const MyNav = ({ title, location }: MyNavProps & RouteComponentProps ) => (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Link to="/">
+            <Navbar.Brand>{title}</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+                <Link to="/">
+                    <div className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</div>
+                </Link>
+                <Link to="/menu">
+                    <div className={location.pathname === '/menu' ? 'nav-link active' : 'nav-link'}>Menu</div>
+                </Link>
+                <Link to="/reservations">
+                    <div className={location.pathname === '/reservations' ? 'nav-link active' : 'nav-link'}>Reservation</div>
+                </Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+)
+
+export default withRouter(MyNav)

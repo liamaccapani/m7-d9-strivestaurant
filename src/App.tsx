81f0289cffd0451386859374e98b5d0937a4ce75
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-10-28 16:34:03 +0200

    everything working

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import MyNav from './components/MyNav'
 import Home from './components/Home'
 import Reservations from './components/Reservations'
-// import Menu from './components/Menu'
+import Menu from './components/Menu'
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <MyNav title="Strivestaurant" />
         <Route exact path="/" render={(routerProps) => <Home {...routerProps} title="Strivestaurant" />} />
         <Route path="/reservations" component={Reservations} />
+        <Route path="/menu" component={Menu} />
       </Router>
     </div>
   );
